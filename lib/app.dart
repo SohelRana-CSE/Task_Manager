d94224a212673a646eaeb7829ef6ddadd8e6b2b4
Dart_Basic_Practice <soheldcc18@gmail.com>
2025-03-04 00:06:24 +0600

    auth files commit

---
+++ lib/app.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:task_manager/ui/screens/auth/splash_screen.dart';
+import 'package:task_manager/ui/utility/app_colors.dart';
+
+class TaskManagerApp extends StatelessWidget {
+  const TaskManagerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(),
+      theme: lightThemeData(),
+    );
+  }
+
+  ThemeData lightThemeData() {
+    return ThemeData(
+      inputDecorationTheme: InputDecorationTheme(
+        fillColor: Colors.white,
+        filled: true,
+        hintStyle: TextStyle(color: Colors.grey.shade400),
+        border: OutlineInputBorder(borderSide: BorderSide.none),
+      ),
+      textTheme: const TextTheme(
+        titleLarge: TextStyle(
+          fontSize: 28,
+          fontWeight: FontWeight.bold,
+          color: Colors.black
+        ),
+        titleSmall: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.w500,
+          color: Colors.grey,
+          letterSpacing: 0.4
+        ),
+      ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.themeColor,
+          padding: EdgeInsets.symmetric(vertical: 12),
+          foregroundColor: AppColors.white,
+          fixedSize: Size.fromWidth(double.maxFinite),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+      textButtonTheme: TextButtonThemeData(
+        style: TextButton.styleFrom(
+          foregroundColor: Colors.grey,
+          textStyle: const TextStyle(
+            fontWeight: FontWeight.w600
+          )
+        )
+      )
+    );
+  }
+}
